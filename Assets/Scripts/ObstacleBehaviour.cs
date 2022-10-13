d0d071e8a35ce096271249fc3a4529fb71d3a58b
arCnuts <111523660+arCnuts@users.noreply.github.com>
2022-10-13 15:29:16 +0200

    Initial commit

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.tag = "obstacle";
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private int i;
+    public float forwardSpeed;
+    public float moveSpeed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);  
+
+        if(Input.GetKeyDown(KeyCode.A) && i == 0 | i == 1)
+        {
+            --i;
+            transform.Translate(Vector3.left * moveSpeed);
+        }    
+        
+        if(Input.GetKeyDown(KeyCode.D) && i == 0 | i == -1)
+        {
+            ++i;
+            transform.Translate(Vector3.right * moveSpeed);
+        }    
+    }
+
+
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        if(collision.gameObject.tag == "obstacle")
+        {
+            gameObject.transform.position = Vector3.zero;
+            i =+ 0;
+        }
+    }
+}
