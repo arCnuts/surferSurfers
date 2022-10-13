d4ad75d4072b4d1e3afb8aa438c1c480654f9bae
arCnuts <111523660+arCnuts@users.noreply.github.com>
2022-10-13 18:31:42 +0200

    1

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] tilePrefabs;
+
+    private Transform playerTransform;
+    private float spawnZ = 0;
+    private int amnTilesOnScreen = 7;
+    public float tileLength;
+
+
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for (int i = 0; i < amnTilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(playerTransform.position.z > (spawnZ - amnTilesOnScreen * tileLength))
+        {
+            SpawnTile();
+        }
     }
+
+    void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate (tilePrefabs [0]) as GameObject;
+        go.transform.SetParent (transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+    }
+
 }
