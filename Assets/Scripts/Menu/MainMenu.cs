569e445ae0bf6b906a586233a2f12f97c07892ab
arCnuts <111523660+arCnuts@users.noreply.github.com>
2022-10-27 12:53:44 +0200

    Update MainMenu.cs

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -4,29 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour{
-    public void PlayGame ()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  public void PlayGame ()
+  {
+  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
 
-    }
-
-        public void QuitGame ()
-        {
-            
-
-        Debug.Log("QUIT!");
-        Application.Quit();
-        }
-
-    
-
+  public void QuitGame ()
+  {
+  Application.Quit();
+  }
 
 }
-
-
-
-
-
-
-
-  
