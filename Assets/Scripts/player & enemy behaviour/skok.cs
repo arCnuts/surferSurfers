ade9f1cc7569f6120b979b93e6264b22c024e46d
Qwesak <111525049+Qwesak@users.noreply.github.com>
2022-12-12 00:49:52 +0100

    idespacdobrasnoc

---
+++ Assets/Scripts/player & enemy behaviour/skok.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skok : MonoBehaviour
+{
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
+
